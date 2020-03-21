bac19824b3bd7825020900e4d2fe855f6661d111
thiagoac33 <aquino.thiago33@gmail.com>
2020-03-21 11:10:12 -0300

    Initial commit for the repository

---
+++ firstNodeCode.js
@@ -0,0 +1,10 @@
+let heyShowMeSomething = true;
+let showThis = 'I love coding!';
+
+const showMeThisNow = () => {
+    if(heyShowMeSomething){
+        return console.log(showThis);
+    }
+}
+
+showMeThisNow();
